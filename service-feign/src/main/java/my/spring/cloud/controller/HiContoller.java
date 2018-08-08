bcd677c226abb1270b81bf9d8a6ade04100eff3e
AEROINFO\yao.dong <yao.dong@aeroinfo.com>
2018-08-07 20:29:05 -0700

    service-feign

---
+++ service-feign/src/main/java/my/spring/cloud/controller/HiContoller.java
@@ -0,0 +1,19 @@
+package my.spring.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import my.spring.cloud.service.SchedualServiceHi;
+
+@RestController
+public class HiContoller {
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @GetMapping(value = "/hi")
+    public String sayHi(@RequestParam String name) {
+        return schedualServiceHi.sayHiFromClientOne( name );
+    }
+}
---
+++ service-feign/src/main/java/my/spring/cloud/service/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package my.spring.cloud.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+	@Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
