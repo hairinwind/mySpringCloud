55135d1fae1130f5f49169b3891e3b7df5478d12
Yao Dong <ydong@luxoft.com>
2019-12-25 10:52:13 -0800

    check in

---
+++ spring-gateway/README.md
@@ -0,0 +1,5 @@
+## reference doc
+https://blog.csdn.net/forezp/article/details/83792388  
+
+https://spring.io/guides/gs/gateway/
+
---
+++ spring-gateway/src/main/java/my/spring/springgateway/SpringGatewayApplication.java
@@ -0,0 +1,45 @@
+package my.spring.springgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@EnableConfigurationProperties(UriConfiguration.class)
+@RestController
+public class SpringGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+        String httpUri = uriConfiguration.getHttpbin();
+//                "http://httpbin.org:80";
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri(httpUri))
+                .route(p -> p
+                        .host("*.hystrix.com")
+                        .filters(f -> f
+                                .hystrix(config -> config
+                                        .setName("mycmd")
+                                        .setFallbackUri("forward:/fallback")))
+                        .uri(httpUri))
+                .build();
+    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+}
